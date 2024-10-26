b5b77312a751fd6c5ab1260ca5ba50aba891bbd7
Ahmed Maher <ahmed.maher.sallam@vultara.com>
2024-10-26 18:46:09 +0300

    add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { OrganizationModule } from './organization/organization.module';
+import * as dotenv from 'dotenv';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from './redis/redis.module';
+
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule, 
+    UserModule, 
+    OrganizationModule, RedisModule
+  ],
+  providers: [],
 })
 export class AppModule {}
